346032c7dd4be16bdc59815027beb14e5240d242
Thomas Hillyer <thomashillyer@intranet.solutionsmodex.com>
2018-06-28 13:05:35 -0400

    full calendar hacky component

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ import Waypoint from 'react-waypoint'
 import Header from '../components/Header'
 import Nav from '../components/Nav'
 import pic01 from '../assets/images/pic01.jpg'
+import Calendar from '../components/Calendar'
 
 class Index extends React.Component {
   constructor(props) {
@@ -66,6 +67,7 @@ class Index extends React.Component {
           <section id="first" className="main special">
             <header className="major">
               <h2>Schedule</h2>
+              <Calendar />
             </header>
 
             
