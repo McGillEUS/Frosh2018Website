f4e0a4e51a9dbf9d72164b0fed4d9b3a2fff910c
ajaypatel24 <35381714+ajaypatel24@users.noreply.github.com>
2018-08-08 19:27:23 -0400

    Removed flexbox because its a piece of garbage
    AHHHHHHHHH css wont work properly once thats done its done

---
+++ src/components/LeaderPage.js
@@ -0,0 +1,403 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import get from 'lodash/get'
+import Helmet from 'react-helmet'
+
+import HeaderLeaders from '../components/HeaderLeaders'
+import leader from '../assets/images/leaders/one.png';
+import {Column, Row} from 'simple-flexbox';
+
+const LeaderPage = (props) => (
+
+
+  <div>
+    <Helmet title={get(this, 'props.data.site.siteMetadata.title')} />
+
+   
+    <div id="main">
+      <section id="content" className="main">
+        
+        <div class = "row">
+
+
+
+          <div class = "group">
+            <p> Group 1 </p>
+          </div>
+
+          <div class = "column">
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />            
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />               
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+          </div>
+
+          <div class = "group">
+            <p> Group 1 </p>
+          </div>
+
+          <div class = "column">
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />            
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />          
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+          </div>
+
+          <div class = "group">
+            <p> Group 1 </p>
+          </div>
+
+          <div class = "column">
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />            
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />          
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+          </div>
+
+          <div class = "group">
+            <p> Group 1 </p>
+          </div>
+
+
+          <div class = "column">
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />            
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />          
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+          </div>
+
+          <div class = "group">
+            <p> Group 1 </p>
+          </div>
+
+          <div class = "column">
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />            
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />          
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+          </div>
+
+          <div class = "group">
+            <p> Group 1 </p>
+          </div>
+
+          <div class = "column">
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />            
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />          
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+          </div>
+
+          <div class = "group">
+            <p> Group 1 </p>
+          </div>
+
+          <div class = "column">
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />            
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />          
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+          </div>
+
+          <div class = "group">
+            <p> Group 1 </p>
+          </div>
+
+          <div class = "column">
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />            
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />          
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+          </div>
+
+          <div class = "group">
+            <p> Group 1 </p>
+          </div>
+
+          <div class = "column">
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />            
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />          
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+          </div>
+
+          <div class = "group">
+            <p> Group 1 </p>
+          </div>
+
+          <div class = "column">
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />            
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />          
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+          </div>
+
+          <div class = "group">
+            <p> Group 1 </p>
+          </div>
+
+          <div class = "column">
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />            
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />          
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+          </div>
+
+          <div class = "group">
+            <p> Group 1 </p>
+          </div>
+
+          <div class = "column">
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />            
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />          
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+          </div>
+
+          <div class = "group">
+            <p> Group 1 </p>
+          </div>
+
+          <div class = "column">
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />            
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />          
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+          </div>
+
+          <div class = "group">
+            <p> Group 1 </p>
+          </div>
+
+
+          <div class = "column">
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />            
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />          
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+          </div>
+
+          <div class = "group">
+            <p> Group 1 </p>
+          </div>
+
+          <div class = "column">
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />            
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />          
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+          </div>
+
+          <div class = "group">
+            <p> Group 1 </p>
+          </div>
+
+          <div class = "column">
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />            
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />          
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+          </div>
+
+          <div class = "group">
+            <p> Group 1 </p>
+          </div>
+
+          <div class = "column">
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />            
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />          
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+          </div>
+
+          <div class = "group">
+            <p> Group 1 </p>
+          </div>
+
+          <div class = "column">
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />            
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />          
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+          </div>
+
+          <div class = "group">
+            <p> Group 1 </p>
+          </div>
+
+          <div class = "column">
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />            
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />          
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+          </div>
+
+          <div class = "group">
+            <p> Group 1 </p>
+          </div>
+
+          <div class = "column">
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />            
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />          
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+          </div>
+
+          <div class = "group">
+            <p> Group 1 </p>
+          </div>
+
+          <div class = "column">
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />            
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />          
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+          </div>
+
+          <div class = "group">
+            <p> Group 1 </p>
+          </div>
+
+          <div class = "column">
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />            
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />          
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+          </div>
+
+          <div class = "group">
+            <p> Group 1 </p>
+          </div>
+
+          <div class = "column">
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />            
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />          
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+          </div>
+
+          <div class = "group">
+            <p> Group 1 </p>
+          </div>
+
+          <div class = "column">
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />            
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />          
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+          </div>
+
+          <div class = "group">
+            <p> Group 1 </p>
+          </div>
+
+          <div class = "column">
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />            
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />          
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+          </div>
+
+          <div class = "group">
+            <p> Group 1 </p>
+          </div>
+
+          <div class = "column">
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />            
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />          
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+          </div>
+
+          <div class = "group">
+            <p> Group 1 </p>
+          </div>
+
+          <div class = "column">
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />            
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />          
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+          </div>
+
+          <div class = "group">
+            <p> Group 1 </p>
+          </div>
+
+          <div class = "column">
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />            
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />          
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+          </div>
+
+          <div class = "group">
+            <p> Group 1 </p>
+          </div>
+
+          <div class = "column">
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />            
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />          
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+          </div>
+
+          <div class = "group">
+            <p> Group 1 </p>
+          </div>
+
+          <div class = "column">
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />            
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />          
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+          </div>
+
+          <div class = "group">
+            <p> Group 1 </p>
+          </div>
+
+          <div class = "column">
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />            
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />          
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+            <img src={leader} style={{ margin: '3rem 2rem', maxWidth: 100 }} alt="" />
+          </div>
+
+
+
+            
+          </div>
+    
+        </section>
+
+      </div>
+    </div>
+          
+)
+
+export default LeaderPage
+
+
+
+
+
+
+
+        
+
+    
+    
+        
+          
+
+
+          
+
+        
+      
+
+    
+  
+
+
