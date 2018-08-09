1132f287935f3a98eb20450e8a7dfd3f75888bf9
ajaypatel24 <35381714+ajaypatel24@users.noreply.github.com>
2018-08-09 15:58:48 -0400

    bios looking ok?
    still some problems with uneven spacing but thats easily fixable

--- src/components/LeaderPage.js
+++ src/components/LeaderPage.js
@@ -35,380 +35,436 @@ const LeaderPage = (props) => (
     <div id="main">
       <section id="content" className="main">
         
-        <div class = "row">
+      <div id = "row">
 
 
 
 
-        <div class = "group">
+          <div id = "group">
           <h2> Coordinators </h2> 
           </div>
 
         
-          <div class = "column">
-            <img src={AlexH} style={{  }} alt="" />          
-            <img src={AnnaB} style={{  }} alt="" />               
-            <img src={AnnaH} style={{  }} alt="" />
-            <img src={Canon} style={{  }} alt="" />
+          <div id = "coord">
+            <div id = "item">
+            <img src={AlexH} style={{  }} alt="" />     
+            <span id="caption">Alex Hale</span> 
+            </div>
+
+            <div id = "item">
+            <img src={AnnaH} style={{  }} alt="" />     
+            <span id="caption">Anna H</span>
+            </div> 
+
+            <div id = "item">
+            <img src={AnnaB} style={{  }} alt="" />     
+            <span id="caption">Anna Bieber</span>
+            </div> 
+
+            <div id = "item">
+            <img src={Canon} style={{  }} alt="" />     
+            <span id="caption">Canon</span>
+            </div> 
+
+            <div id = "item">
+            <img src={David} style={{  }} alt="" />     
+            <span id="caption">David</span> 
+            </div>
+
+            <div id = "item">
+            <img src={Ece} style={{  }} alt="" />     
+            <span id="caption">Ece Pidik</span>
+            </div> 
+
+            <div id = "item">
+            <img src={Ella} style={{  }} alt="" />     
+            <span id="caption">Ella</span>
+            </div> 
+
+            <div id = "item">
+            <img src={Kevin} style={{  }} alt="" />     
+            <span id="caption">Kevin</span>
+            </div> 
+
+            <div id = "item">
+            <img src={Laurent} style={{  }} alt="" />     
+            <span id="caption">Laurent</span> 
+            </div>
+
+            <div id = "item">
+            <img src={Nick} style={{  }} alt="" />     
+            <span id="caption">Nick</span>
+            </div> 
+
+            <div id = "item">
+            <img src={Thomas} style={{  }} alt="" />     
+            <span id="caption">Thomas Hillyer</span>
+            </div> 
+
+            <div id = "item">
+            <img src={Tristan} style={{  }} alt="" />     
+            <span id="caption">Tristan</span>
+            </div> 
+
           </div>
 
-
-          <div class = "column">
-            <img src={David} style={{  }} alt="" />            
-            <img src={Ece} style={{  }} alt="" />               
-            <img src={Ella} style={{  }} alt="" />
-            <img src={Kevin} style={{  }} alt="" />
-          </div>
-
-          <div class = "column">
-            <img src={Laurent} style={{  }} alt="" />            
-            <img src={Nick} style={{  }} alt="" />               
-            <img src={Thomas} style={{  }} alt="" />
-            <img src={Tristan} style={{  }} alt="" />
-          </div>
+            
+          
           
 
 
 
-          <div class = "group">
+          <div id = "group">
           <h2> Group 1 </h2> 
           </div>
 
         
-          <div class = "column">
-            <img src={leader} style={{  }} alt="" />            
-            <img src={leader} style={{  }} alt="" />               
-            <img src={leader} style={{  }} alt="" />
-            <img src={leader} style={{  }} alt="" />
-          </div>
+          <div id = "item">
+            <img src={Tristan} style={{  }} alt="" />     
+            <span id="caption">sample bio showing that the text stays below the image so long as a word isnt way too long, but that problem most likely wont occur unless someone thinks theyre shakespeare</span>
+          </div> 
+          <div id = "item">
+            <img src={Tristan} style={{  }} alt="" />     
+            <span id="caption">sample bio showing that the text stays below the image so long as a word isnt way too long, but that problem most likely wont occur unless someone thinks theyre shakespeare</span>
+          </div> 
+          <div id = "item">
+            <img src={Tristan} style={{  }} alt="" />     
+            <span id="caption">sample bio showing that the text stays below the image so long as a word isnt way too long, but that problem most likely wont occur unless someone thinks theyre shakespeare</span>
+          </div> 
+          <div id = "item">
+            <img src={Tristan} style={{  }} alt="" />     
+            <span id="caption">sample bio showing that the text stays below the image so long as a word isnt way too long, but that problem most likely wont occur unless someone thinks theyre shakespeare</span>
+          </div> 
+        
 
 
           
             <h2> Group 2 </h2>
           
 
-          <div class = "column">
+          <div id = "column">
+
             <img src={leader} style={{ }} alt="" />            
             <img src={leader} style={{ }} alt="" />          
             <img src={leader} style={{ }} alt="" />
             <img src={leader} style={{ }} alt="" />
           </div>
 
-          <div class = "group">
+          <div id = "group">
             <h2> Group 3 </h2>
           </div>
 
-          <div class = "column">
+          <div id = "column">
             <img src={leader} style={{ }} alt="" />            
             <img src={leader} style={{ }} alt="" />          
             <img src={leader} style={{ }} alt="" />
             <img src={leader} style={{ }} alt="" />
           </div>
 
-          <div class = "group">
+          <div id = "group">
             <h2> Group 4 </h2>
           </div>
 
 
-          <div class = "column">
+          <div id = "column">
             <img src={leader} style={{ }} alt="" />            
             <img src={leader} style={{ }} alt="" />          
             <img src={leader} style={{ }} alt="" />
             <img src={leader} style={{ }} alt="" />
           </div>
 
-          <div class = "group">
+          <div id = "group">
             <h2> Group 5 </h2>
           </div>
 
-          <div class = "column">
+          <div id = "column">
             <img src={leader} style={{ }} alt="" />            
             <img src={leader} style={{ }} alt="" />          
             <img src={leader} style={{ }} alt="" />
             <img src={leader} style={{ }} alt="" />
           </div>
 
-          <div class = "group">
+          <div id = "group">
             <h2> Group 6 </h2>
           </div>
 
-          <div class = "column">
+          <div id = "column">
             <img src={leader} style={{ }} alt="" />            
             <img src={leader} style={{ }} alt="" />          
             <img src={leader} style={{ }} alt="" />
             <img src={leader} style={{ }} alt="" />
           </div>
 
-          <div class = "group">
+          <div id = "group">
             <h2> Group 7 </h2>
           </div>
 
-          <div class = "column">
+          <div id = "column">
             <img src={leader} style={{ }} alt="" />            
             <img src={leader} style={{ }} alt="" />          
             <img src={leader} style={{ }} alt="" />
             <img src={leader} style={{ }} alt="" />
           </div>
 
-          <div class = "group">
+          <div id = "group">
             <h2> Group 8 </h2>
           </div>
 
-          <div class = "column">
+          <div id = "column">
             <img src={leader} style={{ }} alt="" />            
             <img src={leader} style={{ }} alt="" />          
             <img src={leader} style={{ }} alt="" />
             <img src={leader} style={{ }} alt="" />
           </div>
 
-          <div class = "group">
+          <div id = "group">
             <h2> Group 9 </h2>
           </div>
 
-          <div class = "column">
+          <div id = "column">
             <img src={leader} style={{ }} alt="" />            
             <img src={leader} style={{ }} alt="" />          
             <img src={leader} style={{ }} alt="" />
             <img src={leader} style={{ }} alt="" />
           </div>
 
-          <div class = "group">
+          <div id = "group">
             <h2> Group 10 </h2>
           </div>
 
-          <div class = "column">
+          <div id = "column">
             <img src={leader} style={{ }} alt="" />            
             <img src={leader} style={{ }} alt="" />          
             <img src={leader} style={{ }} alt="" />
             <img src={leader} style={{ }} alt="" />
           </div>
 
-          <div class = "group">
+          <div id = "group">
             <h2> Group 11</h2>
           </div>
 
-          <div class = "column">
+          <div id = "column">
             <img src={leader} style={{ }} alt="" />            
             <img src={leader} style={{ }} alt="" />          
             <img src={leader} style={{ }} alt="" />
             <img src={leader} style={{ }} alt="" />
           </div>
 
-          <div class = "group">
+          <div id = "group">
             <h2> Group 12 </h2>
           </div>
 
-          <div class = "column">
+          <div id = "column">
             <img src={leader} style={{ }} alt="" />            
             <img src={leader} style={{ }} alt="" />          
             <img src={leader} style={{ }} alt="" />
             <img src={leader} style={{ }} alt="" />
           </div>
 
-          <div class = "group">
+          <div id = "group">
             <h2> Group 13 </h2>
           </div>
 
-          <div class = "column">
+          <div id = "column">
             <img src={leader} style={{ }} alt="" />            
             <img src={leader} style={{ }} alt="" />          
             <img src={leader} style={{ }} alt="" />
             <img src={leader} style={{ }} alt="" />
           </div>
 
-          <div class = "group">
+          <div id = "group">
             <h2> Group 14 </h2>
           </div>
 
 
-          <div class = "column">
+          <div id = "column">
             <img src={leader} style={{ }} alt="" />            
             <img src={leader} style={{ }} alt="" />          
             <img src={leader} style={{ }} alt="" />
             <img src={leader} style={{ }} alt="" />
           </div>
 
-          <div class = "group">
+          <div id = "group">
             <h2> Group 15 </h2>
           </div>
 
-          <div class = "column">
+          <div id = "column">
             <img src={leader} style={{ }} alt="" />            
             <img src={leader} style={{ }} alt="" />          
             <img src={leader} style={{ }} alt="" />
             <img src={leader} style={{ }} alt="" />
           </div>
 
-          <div class = "group">
+          <div id = "group">
             <h2> Group 16 </h2>
           </div>
 
-          <div class = "column">
+          <div id = "column">
             <img src={leader} style={{ }} alt="" />            
             <img src={leader} style={{ }} alt="" />          
             <img src={leader} style={{ }} alt="" />
             <img src={leader} style={{ }} alt="" />
           </div>
 
-          <div class = "group">
+          <div id = "group">
             <h2> Group 17 </h2>
           </div>
 
-          <div class = "column">
+          <div id = "column">
             <img src={leader} style={{ }} alt="" />            
             <img src={leader} style={{ }} alt="" />          
             <img src={leader} style={{ }} alt="" />
             <img src={leader} style={{ }} alt="" />
           </div>
 
-          <div class = "group">
+          <div id = "group">
             <h2> Group 18 </h2>
           </div>
 
-          <div class = "column">
+          <div id = "column">
             <img src={leader} style={{ }} alt="" />            
             <img src={leader} style={{ }} alt="" />          
             <img src={leader} style={{ }} alt="" />
             <img src={leader} style={{ }} alt="" />
           </div>
 
-          <div class = "group">
+          <div id = "group">
             <h2> Group 19 </h2>
           </div>
 
-          <div class = "column">
+          <div id = "column">
             <img src={leader} style={{ }} alt="" />            
             <img src={leader} style={{ }} alt="" />          
             <img src={leader} style={{ }} alt="" />
             <img src={leader} style={{ }} alt="" />
           </div>
 
-          <div class = "group">
+          <div id = "group">
             <h2> Group 20 </h2>
           </div>
 
-          <div class = "column">
+          <div id = "column">
             <img src={leader} style={{ }} alt="" />            
             <img src={leader} style={{ }} alt="" />          
             <img src={leader} style={{ }} alt="" />
             <img src={leader} style={{ }} alt="" />
           </div>
 
-          <div class = "group">
+          <div id = "group">
             <h2> Group 21 </h2>
           </div>
 
-          <div class = "column">
+          <div id = "column">
             <img src={leader} style={{ }} alt="" />            
             <img src={leader} style={{ }} alt="" />          
             <img src={leader} style={{ }} alt="" />
             <img src={leader} style={{ }} alt="" />
           </div>
 
-          <div class = "group">
+          <div id = "group">
             <h2> Group 22 </h2>
           </div>
 
-          <div class = "column">
+          <div id = "column">
             <img src={leader} style={{ }} alt="" />            
             <img src={leader} style={{ }} alt="" />          
             <img src={leader} style={{ }} alt="" />
             <img src={leader} style={{ }} alt="" />
           </div>
 
-          <div class = "group">
+          <div id = "group">
             <h2> Group 23 </h2>
           </div>
 
-          <div class = "column">
+          <div id = "column">
             <img src={leader} style={{ }} alt="" />            
             <img src={leader} style={{ }} alt="" />          
             <img src={leader} style={{ }} alt="" />
             <img src={leader} style={{ }} alt="" />
           </div>
 
-          <div class = "group">
+          <div id = "group">
             <h2> Group 24 </h2>
           </div>
 
-          <div class = "column">
+          <div id = "column">
             <img src={leader} style={{ }} alt="" />            
             <img src={leader} style={{ }} alt="" />          
             <img src={leader} style={{ }} alt="" />
             <img src={leader} style={{ }} alt="" />
           </div>
 
-          <div class = "group">
+          <div id = "group">
             <h2> Group 25 </h2>
           </div>
 
-          <div class = "column">
+          <div id = "column">
             <img src={leader} style={{ }} alt="" />            
             <img src={leader} style={{ }} alt="" />          
             <img src={leader} style={{ }} alt="" />
             <img src={leader} style={{ }} alt="" />
           </div>
 
-          <div class = "group">
+          <div id = "group">
             <h2> Group 26 </h2>
           </div>
 
-          <div class = "column">
+          <div id = "column">
             <img src={leader} style={{ }} alt="" />            
             <img src={leader} style={{ }} alt="" />          
             <img src={leader} style={{ }} alt="" />
             <img src={leader} style={{ }} alt="" />
           </div>
 
-          <div class = "group">
+          <div id = "group">
             <h2> Group 27 </h2>
           </div>
 
-          <div class = "column">
+          <div id = "column">
             <img src={leader} style={{ }} alt="" />            
             <img src={leader} style={{ }} alt="" />          
             <img src={leader} style={{ }} alt="" />
             <img src={leader} style={{ }} alt="" />
           </div>
 
-          <div class = "group">
+          <div id = "group">
             <h2> Group 28 </h2>
           </div>
 
-          <div class = "column">
+          <div id = "column">
             <img src={leader} style={{ }} alt="" />            
             <img src={leader} style={{ }} alt="" />          
             <img src={leader} style={{ }} alt="" />
             <img src={leader} style={{ }} alt="" />
           </div>
 
-          <div class = "group">
+          <div id = "group">
             <h2> Group 29 </h2>
           </div>
 
-          <div class = "column">
+          <div id = "column">
             <img src={leader} style={{ }} alt="" />            
             <img src={leader} style={{ }} alt="" />          
             <img src={leader} style={{ }} alt="" />
             <img src={leader} style={{ }} alt="" />
           </div>
 
-          <div class = "group">
+          <div id = "group">
             <h2> Group 30 </h2>
           </div>
 
-          <div class = "column">
+          <div id = "column">
             <img src={leader} style={{ }} alt="" />            
             <img src={leader} style={{ }} alt="" />          
             <img src={leader} style={{ }} alt="" />
             <img src={leader} style={{ }} alt="" />
           </div>
 
-          <div class = "group">
+          <div id = "group">
             <h2> Group 1 </h2>
           </div>
 
-          <div class = "column">
+          <div id = "column">
             <img src={leader} style={{ }} alt="" />            
             <img src={leader} style={{ }} alt="" />          
             <img src={leader} style={{ }} alt="" />
