5f084e6afc04c46d2d2a976c053bf4473ca7f5e9
ajaypatel24 <35381714+ajaypatel24@users.noreply.github.com>
2018-08-16 18:04:40 -0400

    all photos are now there
    all photos imported, some are missing from the actual google sheets file so idk what to do about those for now

--- src/components/LeaderPage.js
+++ src/components/LeaderPage.js
@@ -42,10 +42,6 @@ import Mike from '../assets/images/leaders/group 3/Capture - Mike Chan (1).png';
 import Ahmad from '../assets/images/leaders/group 3/Ahmad Masri.jpg';
 
 //group 4 REDOWNLOAD BOYZ
-import SarahF from '../assets/images/leaders/group 2/IMG_3959 - Sarah Ford.jpeg';
-import Nicolas from '../assets/images/leaders/group 2/IMG_4321 - nicolas siriwardhana.jpg';
-import Kabir from '../assets/images/leaders/group 2/Kabir Sethi.jpg';
-import Shaun from '../assets/images/leaders/group 2/Shaun Jiji.jpeg';
 
 //group 5
 import BenjaminC from '../assets/images/leaders/group 5/Benjamin Carris.jpg';
@@ -87,70 +83,106 @@ import Mat from '../assets/images/leaders/group 10/IMG_20180603_184017_Bokeh (2)
 import Laurence from '../assets/images/leaders/group 10/Laurence Peinturier.jpg';
 
 //group 11
-import Thierry from '../assets/images/leaders/group 2/IMG_3687 - Thierry Pichette.jpg';
-import Maxence from '../assets/images/leaders/group 2/Maxence Frenette.jpg';
-import Marie-Claire from '../assets/images/leaders/group 2/photo2018 - Marie-Claire Hamel.jpg';
-import TristanBouchard from '../assets/images/leaders/group 2/Tristan Bouchard.jpg';
+import Thierry from '../assets/images/leaders/group 11/IMG_3687 - Thierry Pichette.jpg';
+import Maxence from '../assets/images/leaders/group 11/Maxence Frenette.jpg';
+import MarieClaire from '../assets/images/leaders/group 11/photo2018 - Marie-Claire Hamel.jpg';
+import TristanBouchard from '../assets/images/leaders/group 11/Tristan Bouchard.jpg';
 
-//group 11
-import Thierry from '../assets/images/leaders/group 2/IMG_3687 - Thierry Pichette.jpg';
-import Maxence from '../assets/images/leaders/group 2/Maxence Frenette.jpg';
-import Marie-Claire from '../assets/images/leaders/group 2/photo2018 - Marie-Claire Hamel.jpg';
-import TristanBouchard from '../assets/images/leaders/group 2/Tristan Bouchard.jpg';
+//group 12
+import AlexGruenwald from '../assets/images/leaders/group 12/Alex Gruenwald (1).jpg';
+import Cheng from '../assets/images/leaders/group 12/Cheng Lin.jpg';
+import Jonathan from '../assets/images/leaders/group 12/Jonathan Ong.jpg';
+import K from '../assets/images/leaders/group 12/K Marshall.jpg';
+import Preyansh from '../assets/images/leaders/group 12/Preyansh Kaushik.jpg';
 
-//group 11
-import Thierry from '../assets/images/leaders/group 2/IMG_3687 - Thierry Pichette.jpg';
-import Maxence from '../assets/images/leaders/group 2/Maxence Frenette.jpg';
-import Marie-Claire from '../assets/images/leaders/group 2/photo2018 - Marie-Claire Hamel.jpg';
-import TristanBouchard from '../assets/images/leaders/group 2/Tristan Bouchard.jpg';
+//group 13
+import Joel from '../assets/images/leaders/group 13/image - Joel.jpg';
+import RyanZel from '../assets/images/leaders/group 13/IMG_9717 - Ryan Zelnicker.jpg';
+import Tal from '../assets/images/leaders/group 13/IMG-1463 - Tal Abravanel.jpg';
+import Michel from '../assets/images/leaders/group 13/photo bam event - Michel Abdel Nour.jpg';
 
-//group 11
-import Thierry from '../assets/images/leaders/group 2/IMG_3687 - Thierry Pichette.jpg';
-import Maxence from '../assets/images/leaders/group 2/Maxence Frenette.jpg';
-import Marie-Claire from '../assets/images/leaders/group 2/photo2018 - Marie-Claire Hamel.jpg';
-import TristanBouchard from '../assets/images/leaders/group 2/Tristan Bouchard.jpg';
+//group 14
+import eleri from '../assets/images/leaders/group 14/eleri m.jpg';
+import AlexScheffel from '../assets/images/leaders/group 14/FroshPhoto - Alex Scheffel.jpg';
+import Ian from '../assets/images/leaders/group 14/ixgauth1.jpg';
+//import Michel from '../assets/images/leaders/group 12/photo bam event - Michel Abdel Nour.jpg';
 
-//group 11
-import Thierry from '../assets/images/leaders/group 2/IMG_3687 - Thierry Pichette.jpg';
-import Maxence from '../assets/images/leaders/group 2/Maxence Frenette.jpg';
-import Marie-Claire from '../assets/images/leaders/group 2/photo2018 - Marie-Claire Hamel.jpg';
-import TristanBouchard from '../assets/images/leaders/group 2/Tristan Bouchard.jpg';
+//group 15
+import annejulie from '../assets/images/leaders/group 15/anne-julie c.jpg';
+import Emeric from '../assets/images/leaders/group 15/ic-1682 - Emeric Bernier.jpg';
+import Lara from '../assets/images/leaders/group 15/Lara Ghanem.jpg';
+import Vassil from '../assets/images/leaders/group 15/Pic Vassil - Vassil Kroumov.png';
+import Mario from '../assets/images/leaders/group 15/pic - Mario Lipari.jpg';
 
-//group 11
-import Thierry from '../assets/images/leaders/group 2/IMG_3687 - Thierry Pichette.jpg';
-import Maxence from '../assets/images/leaders/group 2/Maxence Frenette.jpg';
-import Marie-Claire from '../assets/images/leaders/group 2/photo2018 - Marie-Claire Hamel.jpg';
-import TristanBouchard from '../assets/images/leaders/group 2/Tristan Bouchard.jpg';
+//group 16
+import Hugo from '../assets/images/leaders/group 16/20180508_185731 - hugo bernard.jpg';
+import Isabelle from '../assets/images/leaders/group 16/image-2018-07-31 - Isabelle del Mundo.jpg';
+import Reda from '../assets/images/leaders/group 16/IMG_0848 - Reda El Khili.jpg';
+import Oliver from '../assets/images/leaders/group 16/IMG_1992 - Oliver Verzani.jpg';
+import Nima from '../assets/images/leaders/group 16/Nima Chatlani.jpg';
 
-//group 11
-import Thierry from '../assets/images/leaders/group 2/IMG_3687 - Thierry Pichette.jpg';
-import Maxence from '../assets/images/leaders/group 2/Maxence Frenette.jpg';
-import Marie-Claire from '../assets/images/leaders/group 2/photo2018 - Marie-Claire Hamel.jpg';
-import TristanBouchard from '../assets/images/leaders/group 2/Tristan Bouchard.jpg';
+//group 17
+import Christos from '../assets/images/leaders/group 17/Christos Cunning.jpg';
+import Cara from '../assets/images/leaders/group 17/fullsizeoutput_d3d - Zhang Cara.jpeg';
+import Kelly from '../assets/images/leaders/group 17/Kelly Ma.jpg';
+//import Oliver from '../assets/images/leaders/group 16/IMG_1992 - Oliver Verzani.jpg';
 
-//group 11
-import Thierry from '../assets/images/leaders/group 2/IMG_3687 - Thierry Pichette.jpg';
-import Maxence from '../assets/images/leaders/group 2/Maxence Frenette.jpg';
-import Marie-Claire from '../assets/images/leaders/group 2/photo2018 - Marie-Claire Hamel.jpg';
-import TristanBouchard from '../assets/images/leaders/group 2/Tristan Bouchard.jpg';
+//group 18
+import AndiCamille from '../assets/images/leaders/group 18/frosh picture (square) - Andi-Camille Batie.jpg';
+import AlexanderTorabi from '../assets/images/leaders/group 18/IMG_3071 - Alexander Torabi.jpg';
+import RosalieGuay from '../assets/images/leaders/group 18/Rosalie Guay.png';
+//import Oliver from '../assets/images/leaders/group 16/IMG_1992 - Oliver Verzani.jpg';
 
-//group 11
-import Thierry from '../assets/images/leaders/group 2/IMG_3687 - Thierry Pichette.jpg';
-import Maxence from '../assets/images/leaders/group 2/Maxence Frenette.jpg';
-import Marie-Claire from '../assets/images/leaders/group 2/photo2018 - Marie-Claire Hamel.jpg';
-import TristanBouchard from '../assets/images/leaders/group 2/Tristan Bouchard.jpg';
+//group 19
+import Amanda from '../assets/images/leaders/group 19/IMG_1766 - Amanda Forsans.jpg';
+import Kevin from '../assets/images/leaders/group 19/Kevin Cole.png';
+import Nicole from '../assets/images/leaders/group 19/Nicole Thoma.jpg';
+import Will from '../assets/images/leaders/group 19/will_connors.jpg';
 
-//group 11
-import Thierry from '../assets/images/leaders/group 2/IMG_3687 - Thierry Pichette.jpg';
-import Maxence from '../assets/images/leaders/group 2/Maxence Frenette.jpg';
-import Marie-Claire from '../assets/images/leaders/group 2/photo2018 - Marie-Claire Hamel.jpg';
-import TristanBouchard from '../assets/images/leaders/group 2/Tristan Bouchard.jpg';
+//group 20
+import Amanda from '../assets/images/leaders/group 20/frosh - nicolas Bieber.jpg';
+import Kevin from '../assets/images/leaders/group 20/IMG_6682 - Alice Lemay.jpg';
+import Nicole from '../assets/images/leaders/group 20/IMG_7500 (2) - Julia R..jpg';
+import Will from '../assets/images/leaders/group 20/Sydney Netto.png';
 
-//group 11
-import Thierry from '../assets/images/leaders/group 2/IMG_3687 - Thierry Pichette.jpg';
-import Maxence from '../assets/images/leaders/group 2/Maxence Frenette.jpg';
-import Marie-Claire from '../assets/images/leaders/group 2/photo2018 - Marie-Claire Hamel.jpg';
-import TristanBouchard from '../assets/images/leaders/group 2/Tristan Bouchard.jpg';
+//group 21
+import Denise from '../assets/images/leaders/group 21/IMG_8257-1 - Denise Bridges.jpg';
+import RyanWong from '../assets/images/leaders/group 21/Ryan Wong - Frosh Leader Bio Picture 2018 - Ryan Wong.jpg';
+import Traven from '../assets/images/leaders/group 21/TravenPhoto - Traven Blaney.jpg';
+import Julia from '../assets/images/leaders/group 21/USLS Conference Photo (2) - Julia Elson.jpg';
+
+//group 22
+import Danny from '../assets/images/leaders/group 22/DSC02721 - Danny Amon.jpg';
+import SarahJ from '../assets/images/leaders/group 22/Frosh pic - Sarah J.jpg';
+import Amy from '../assets/images/leaders/group 22/froshphoto - Amy Hayes.jpg';
+import Khalis from '../assets/images/leaders/group 22/Snowball - Khalis Khaleel.jpeg';
+
+//group 23
+import SamBaker from '../assets/images/leaders/group 23/Frosh Leader Bio - Sam Baker.jpg';
+import Donya from '../assets/images/leaders/group 23/fullsizeoutput_abb - Donya Hojabr.jpeg';
+import Jenny from '../assets/images/leaders/group 23/IMG_9068 - Sam Cattani.jpg';
+import SamCattani from '../assets/images/leaders/group 23/Jenny Li.png';
+
+//group 24
+import Andre from '../assets/images/leaders/group 24/Andre Lee.jpg';
+import David from '../assets/images/leaders/group 24/FL - David Desmangles.jpeg';
+import Shane from '../assets/images/leaders/group 24/IMG_7711 - ShaneRankin9.jpg';
+import Noam from '../assets/images/leaders/group 24/noam rabbani - Noam Rabbani.jpg';
+import AnnaSymon from '../assets/images/leaders/group 24/pic - Anna Symon.png';
+
+//group 25
+import Aidan from '../assets/images/leaders/group 25/frosh_picture2018_proper - Aidan Sullivan.jpg';
+import Paul from '../assets/images/leaders/group 25/index - Paul Fournier.png';
+import Marine from '../assets/images/leaders/group 25/Marine F-Mouras.jpg';
+import Pedro from '../assets/images/leaders/group 25/Pedro Leite.jpg';
+import Zoheir from '../assets/images/leaders/group 25/Zoheir Seddiki.jpg';
+
+//group 26
+import floflo from '../assets/images/leaders/group 26/floflo - Florence Diep.jpg';
+import Julian from '../assets/images/leaders/group 26/IMG_2406 - Julian Pucella.jpg';
+import Ousmane from '../assets/images/leaders/group 26/Ousmane - Ousmane Baricisse.jpg';
+import Vignesh from '../assets/images/leaders/group 26/Vignesh Pushparaj.jpg';
+
 
 const LeaderPage = (props) => (
 
@@ -247,30 +279,33 @@ const LeaderPage = (props) => (
 
         
           <div id = "item">
-            <img src={Tristan} style={{  }} alt="" />    
-            <span id ="name">Tristan</span>
-            <span id = "Q1"> You're on a desert island and can only keep 3 items, what are they? </span>
+            <img src={Kabir} style={{  }} alt="" />    
+            <span id ="name">Kabir Sethi</span>
+            <span id = "Q1"> What 3 things would you bring to a desert island?</span>
+            <span id="caption">Sunglasses, sunscreen, and a sunhat</span>
+            <span id ="Q1">What are you most excited about for Frosh?</span>
+            <span id="caption">Meeting all the new faces in the engineering family</span>
+            <span id="Q1">Why would a Froshie want you as their leader?</span>
+            <span id ="caption">My fanny pack runs deep</span>
+          </div> 
+          <div id = "item">
+            <img src={Nicolas} style={{  }} alt="" /> 
+            <span id ="name">Nicolas</span>    
             <span id="caption">sample bio showing that the text stays below the image so long as a word isnt way too long, but that problem most likely wont occur unless someone thinks theyre shakespeare</span>
-            <span id ="Q2">What are you most excited about for Frosh?</span>
+          </div> 
+          <div id = "item">
+            <img src={SarahF} style={{  }} alt="" /> 
+            <span id ="name">SarahF</span>    
+            <span id="caption">sample bio showing that the text stays below the image so long as a word isnt way too long, but that problem most likely wont occur unless someone thinks theyre shakespeare</span>
+          </div> 
+          <div id = "item">
+            <img src={Shaun} style={{  }} alt="" />  
+            <span id ="name">Shaun</span>   
+            <span id="caption">sample bio showing that the text stays below the image so long as a word isnt way too long, but that problem most likely wont occur unless someone thinks theyre shakespeare</span>
           </div> 
           <div id = "item">
             <img src={Tristan} style={{  }} alt="" /> 
-            <span id ="name">Tristan</span>    
-            <span id="caption">sample bio showing that the text stays below the image so long as a word isnt way too long, but that problem most likely wont occur unless someone thinks theyre shakespeare</span>
-          </div> 
-          <div id = "item">
-            <img src={Tristan} style={{  }} alt="" /> 
-            <span id ="name">Lee Chong Wei</span>    
-            <span id="caption">sample bio showing that the text stays below the image so long as a word isnt way too long, but that problem most likely wont occur unless someone thinks theyre shakespeare</span>
-          </div> 
-          <div id = "item">
-            <img src={Tristan} style={{  }} alt="" />  
-            <span id ="name">Tristan</span>   
-            <span id="caption">sample bio showing that the text stays below the image so long as a word isnt way too long, but that problem most likely wont occur unless someone thinks theyre shakespeare</span>
-          </div> 
-          <div id = "item">
-            <img src={Tristan} style={{  }} alt="" /> 
-            <span id ="name">Lee Chong Wei</span>    
+            <span id ="name">PlaceHolder</span>    
             <span id="caption">sample bio showing that the text stays below the image so long as a word isnt way too long, but that problem most likely wont occur unless someone thinks theyre shakespeare</span>
           </div> 
         
